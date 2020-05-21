dfbb62024d38a45ca7e6d5b88e6b1530a0b76abd
dmesa2 <dmesa2@pdx.edu>
2020-05-20 20:40:22 -0700

    Update LoopDataCleaning.py

--- Code/LoopDataCleaning.py
+++ Code/LoopDataCleaning.py
@@ -13,12 +13,12 @@ cluster = MongoClient("Insert IP Address Here",27017)
 db = cluster["Freeway"]
 collection = db["FreewayLoop"]
 
-remove_speed_zero = collection.update_many({"speed": 0}, { "$unset" : {"speed" : 1 }})
+collection.update_many({"speed": 0}, { "$unset" : {"speed" : 1 }})
 
-remove_speed = collection.update_many({"speed": np.nan}, { "$unset" : {"speed" : 1 }})
+collection.update_many({"speed": np.nan}, { "$unset" : {"speed" : 1 }})
 
-remove_volume = collection.update_many({"volume": np.nan}, { "$unset" : {"volume" : 1 }})
+collection.update_many({"volume": np.nan}, { "$unset" : {"volume" : 1 }})
 
-remove_occupancy = collection.update_many({"occupancy": np.nan}, { "$unset" : {"occupancy" : 1 }})
+collection.update_many({"occupancy": np.nan}, { "$unset" : {"occupancy" : 1 }})
 
 
